3ec164f7ac081f90748fd1e13e48c4be969c4234
Marcin Polonis <marcin.polonis@o2.pl>
2019-07-06 15:18:00 +0200

    Add tests for calculateTotalPoints and for calculateStylePoints

--- calculateStylePoints.test.js
+++ calculateStylePoints.test.js
@@ -34,5 +34,26 @@ describe('stylePoints', () => {
 
             assert.equal(receive, expected);
         });
+        it('should return sum of points 5', () => {
+            const receive = calculateStylePoints([0, 0, 0, 0, 0]);
+
+            const expected = 0;
+
+            assert.equal(receive, expected);
+        });
+        it('should return sum of points 6', () => {
+            const receive = calculateStylePoints([20, 20, 20, 20, 20]);
+
+            const expected = 60;
+
+            assert.equal(receive, expected);
+        });
+        it('should return sum of points 7', () => {
+            const receive = calculateStylePoints([17, 18.5, 19, 19, 20]);
+
+            const expected = 56.5;
+
+            assert.equal(receive, expected);
+        });
     })
 })
--- calculateTotalPoints.test.js
+++ calculateTotalPoints.test.js
@@ -1,10 +1,42 @@
 const assert = require('assert');
-const calculateDistancePoints = require('./calculateDistancePoints');
-const calculateStylePoints = require('./calculateStylePoints');
 const calculateTotalPoints = require('./calculateTotalPoints');
 
 describe('totalPoints', () => {
     describe('numberOfPoints', () => {
-        it('should return ')
-    })
-})
+        it('should return total points for normal hill 1', () => {
+            const receive = calculateTotalPoints(100, 'normal', 98, [19, 18, 19, 20, 17], -5, 3.2)
+
+            const expected = 118.2;
+
+            assert.equal(receive, expected);
+        });
+        it('should return total points for normal hill 2', () => {
+            const receive = calculateTotalPoints(100, 'sth.', 98, [19, 18, 20, 17], null, 3.2)
+
+            const expected = false;
+
+            assert.equal(receive, expected);
+        });
+        it('should return total points for large hill 1', () => {
+            const receive = calculateTotalPoints(134, 'large', 120, [19, 18, 19, 20, 20], -10, 3)
+
+            const expected = 136.2;
+
+            assert.equal(receive, expected);
+        });
+        it('should return total points for large hill 2', () => {
+            const receive = calculateTotalPoints(134, 'large', 120, [19, 18, 19, 20, 20], undefined, 3)
+
+            const expected = false;
+
+            assert.equal(receive, expected);
+        });
+        it('should return total points for flying hill', () => {
+            const receive = calculateTotalPoints(223, 'flying', 200, [19, 18, 19, 20, 20], 3, 5)
+
+            const expected = 213.6;
+
+            assert.equal(receive, expected);
+        })
+    });
+});
